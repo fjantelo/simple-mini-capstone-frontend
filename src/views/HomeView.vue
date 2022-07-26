4d38edd8d70114db36a8eb3ec9f4105b5a8dc160
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-07-26 16:05:05 -0400

    Adds connections to backend API, adds fields to create product, adds products index

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,59 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      products: [],
+      newProductParams: {},
+    };
+  },
+  created: function () {
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function () {
+      axios.get("http://localhost:3000/products.json").then((response) => {
+        this.products = response.data;
+        console.log("All products:", this.products);
+      });
+    },
+    createProduct: function () {
+      axios.post("http://localhost:3000/products.json", this.newProductParams).then((response) => {
+        console.log("Product created", response.data).catch((error) => console.log(error.response));
+        this.products.push(response.data);
+      });
+      this.newProductParams = {};
+    },
   },
 };
 </script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <h2>New Product</h2>
+    <div>
+      Name:
+      <input type="text" v-model="newProductParams.name" />
+      Description:
+      <input type="text" v-model="newProductParams.description" />
+      Price:
+      <input type="text" v-model="newProductParams.price" />
+      Image URL:
+      <input type="text" v-model="newProductParams.image_url" />
+    </div>
+    <button v-on:click="createProduct()">Create Product</button>
+    <div v-for="product in products" v-bind:key="product.id">
+      <h3>{{ product.name }}</h3>
+      <p>${{ product.price }}</p>
+      <img v-bind:src="product.image_url" :alt="product.name" />
+    </div>
+  </div>
+</template>
+
+<style>
+.bluetext {
+  color: blue;
+}
+</style>
