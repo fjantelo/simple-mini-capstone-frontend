63a2ef2bcd40da3198417167667fa23c08a4d328
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-08-01 14:42:01 -0400

    Adds delete product button to show page

--- src/views/ProductsShow.vue
+++ src/views/ProductsShow.vue
@@ -12,7 +12,14 @@ export default {
       this.product = response.data;
     });
   },
-  methods: {},
+  methods: {
+    destroyProduct: function () {
+      axios.delete("/products/" + this.$route.params.id).then((response) => {
+        console.log("Product successfully deleted", response.data);
+        this.$router.push("/products");
+      });
+    },
+  },
 };
 </script>
 
@@ -26,5 +33,7 @@ export default {
     <router-link to="/products">Back to all products</router-link>
     |
     <router-link v-bind:to="`/products/${product.id}/edit`">Edit product</router-link>
+    |
+    <button v-on:click="destroyProduct()">Delete product</button>
   </div>
 </template>
