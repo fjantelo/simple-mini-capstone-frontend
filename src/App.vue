2c3daa35318b23889c2fd4ae5897eb7711655388
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-08-02 14:33:52 -0400

    Adds styling and navbar from boostrap

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,45 @@
 <template>
-  <nav>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">Store</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="/products">All Products</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/products/new">New Product</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/signup">Sign up</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/login">Log in</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">Log out</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+  <!-- Older nav bar -->
+  <!-- <nav>
     <router-link to="/">Home</router-link>
     |
     <router-link to="/products">All Products</router-link>
@@ -11,29 +51,13 @@
     <router-link to="/login">Log in</router-link>
     |
     <router-link to="/logout">Log out</router-link>
-  </nav>
+  </nav> -->
   <router-view />
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  font-family: Century Gothic, CenturyGothic, AppleGothic, sans-serif;
+  background-image: url("./assets/swirl_pattern.webp");
 }
 </style>
